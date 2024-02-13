ba662af5100f93956dd35688300d986241dad52a
joshbam-joshwilly <j.alana@alustudent.com>
2024-02-13 21:13:52 +0100

    product controller

---
+++ src/main/java/com/alana/ecommerceApp/controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.alana.ecommerceApp.controller;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+import com.packt.modern.api.ProductApi;
+import com.packt.modern.api.model.Product;
+import com.packt.modern.api.hateoas.ProductRepresentationModelAssembler;
+import com.packt.modern.api.service.ProductService;
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Joshua Alana
+ **/
+@RestController
+public class ProductController implements ProductApi {
+
+    private ProductService service;
+    private final ProductRepresentationModelAssembler assembler;
+
+    public ProductController(ProductService service, ProductRepresentationModelAssembler assembler) {
+        this.service = service;
+        this.assembler = assembler;
+    }
+
+    @Override
+    public ResponseEntity<Product> getProduct(String id) {
+        return service.getProduct(id).map(assembler::toModel).map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @Override
+    public ResponseEntity<List<Product>> queryProducts(@Valid String tag, @Valid String name,
+                                                       @Valid Integer page, @Valid Integer size) {
+        return ok(assembler.toListModel(service.getAllProducts()));
+    }
+}
