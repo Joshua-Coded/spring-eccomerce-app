c5b94b83ac356521ad04a1d95a1f7c292cf1b9cd
joshbam-joshwilly <j.alana@alustudent.com>
2024-02-14 20:24:17 +0100

    tag entity

---
+++ src/main/java/com/alana/ecommerceApp/entity/TagEntity.java
@@ -0,0 +1,46 @@
+package com.alana.ecommerceApp.entity;
+
+import java.util.UUID;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author : Joshua Alana
+ **/
+@Entity
+@Table(name = "tag")
+public class TagEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ID", updatable = false, nullable = false)
+    private UUID id;
+
+    @NotNull(message = "Product name is required.")
+    @Basic(optional = false)
+    @Column(name = "NAME")
+    private String name;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public TagEntity setId(UUID id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TagEntity setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
