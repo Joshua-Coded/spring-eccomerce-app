114f38aeeb1a58a37db51119042911fb73ce4303
joshbam-joshwilly <j.alana@alustudent.com>
2024-02-14 20:49:29 +0100

    generic error

---
+++ src/main/java/com/alana/ecommerceApp/exception/GenericAlreadyExistsException.java
@@ -0,0 +1,31 @@
+package com.alana.ecommerceApp.exception;
+
+/**
+ * @author : Joshua Alana
+ **/
+public class GenericAlreadyExistsException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+    private final String errMsgKey;
+    private final String errorCode;
+
+    public GenericAlreadyExistsException(ErrorCode code) {
+        super(code.getErrMsgKey());
+        this.errMsgKey = code.getErrMsgKey();
+        this.errorCode = code.getErrCode();
+    }
+
+    public GenericAlreadyExistsException(final String message) {
+        super(message);
+        this.errMsgKey = ErrorCode.RESOURCE_NOT_FOUND.getErrMsgKey();
+        this.errorCode = ErrorCode.RESOURCE_NOT_FOUND.getErrCode();
+    }
+
+    public String getErrMsgKey() {
+        return errMsgKey;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+}
